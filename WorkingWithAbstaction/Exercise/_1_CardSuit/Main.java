0c17580ae4b80f41fd0a475225e798fd29cc734e
Delyana Kadieva <72801355+dkadieva6@users.noreply.github.com>
2022-07-28 05:43:04 +0300

    Add files via upload

---
+++ WorkingWithAbstaction/Exercise/_1_CardSuit/Main.java
@@ -0,0 +1,7 @@
+package OOP.WorkingWithAbstaction.Exercise._1_CardSuit;
+
+public class Main {
+    public static void main(String[] args) {
+        CardSuits.printAll();
+    }
+}
---
+++ WorkingWithAbstaction/Exercise/_2_CardRanks/CardRank.java
@@ -0,0 +1,40 @@
+package OOP.WorkingWithAbstaction.Exercise._2_CardRanks;
+
+public enum CardRank {
+    ACE(0),
+    TWO(1),
+    THREE(2),
+    FOUR(3),
+    FIVE(4),
+    SIX(5),
+    SEVEN(6),
+    EIGHT(7),
+    NINE(8),
+    TEN(9),
+    JACK(10),
+    QUEEN(11),
+    KING(12);
+
+    private int value;
+
+    CardRank(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        //Ordinal value: 0; Name value:
+        return "Ordinal value: " + value + "; Name value: ";
+    }
+
+    public static void printAll() {
+        System.out.println("Card Ranks:");
+        for (CardRank value : CardRank.values()) {
+            System.out.println(value + value.name());
+        }
+    }
+}
